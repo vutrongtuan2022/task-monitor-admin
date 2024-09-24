09a933d9e996f604c068e70e8057cc7453249a94
HTN-UnHappyCODE0 <2051063590@e.tlu.edu.vn>
2024-09-24 13:43:39 +0700

    ff

---
+++ src/components/common/IconCustom/interfaces/index.ts
@@ -0,0 +1,8 @@
+export interface PropsIconCustom {
+	icon: React.ReactNode;
+	tooltip: string;
+	onClick?: () => void;
+	color?: string;
+	href?: string;
+	type?: 'edit' | 'delete' | 'view' | 'create';
+}
