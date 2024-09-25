e94756aef8bf8aad3c5e59663cb1fad211f1caf2
HTN-UnHappyCODE0 <2051063590@e.tlu.edu.vn>
2024-09-25 16:13:45 +0700

    ff

---
+++ src/components/pages/user/UpdateUser/UpdateUser.tsx
@@ -0,0 +1,10 @@
+import React from "react"; 
+
+import {PropsUpdateUser} from "./interfaces"; 
+import styles from "./UpdateUser.module.scss"; 
+
+function UpdateUser({}:PropsUpdateUser) { 
+	return <div>UpdateUser</div>; 
+} 
+
+export default UpdateUser;
