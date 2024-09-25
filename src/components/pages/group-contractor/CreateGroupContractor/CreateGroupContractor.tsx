40774c347877906ea9cf7df9af49fbed8a96e69d
truongdb <engtruong1609@gmail.com>
2024-09-25 14:02:42 +0700

    update

--- src/components/pages/group-contractor/CreateGroupContractor/CreateGroupContractor.tsx
+++ src/components/pages/group-contractor/CreateGroupContractor/CreateGroupContractor.tsx
@@ -1,10 +1,62 @@
-import React from "react"; 
+import React, {useState} from 'react';
 
-import {PropsCreateGroupContractor} from "./interfaces"; 
-import styles from "./CreateGroupContractor.module.scss"; 
+import {PropsCreateGroupContractor} from './interfaces';
+import styles from './CreateGroupContractor.module.scss';
+import {IoClose} from 'react-icons/io5';
+import Button from '~/components/common/Button';
+import {FolderOpen} from 'iconsax-react';
+import Form, {Input} from '~/components/common/Form';
+import TextArea from '~/components/common/Form/components/TextArea';
 
-function CreateGroupContractor({}:PropsCreateGroupContractor) { 
-	return <div>CreateGroupContractor</div>; 
-} 
+function CreateGroupContractor({}: PropsCreateGroupContractor) {
+	const [form, setForm] = useState<{name: string; note: string}>({
+		name: '',
+		note: '',
+	});
+
+	return (
+		<Form form={form} setForm={setForm}>
+			<div className={styles.container}>
+				<h4 className={styles.title}>Thêm mới nhóm nhà thầu</h4>
+				<div className={styles.form}>
+					<Input
+						placeholder='Nhập tên nhóm nhà thầu'
+						name='name'
+						type='text'
+						value={form.name}
+						isRequired
+						label={
+							<span>
+								Tên nhóm nhà thầu <span style={{color: 'red'}}>*</span>
+							</span>
+						}
+					/>
+					<div className={styles.note}>
+						<TextArea name='note' placeholder='Nhập mô tả' label='Mô tả' />
+					</div>
+				</div>
+				<div className={styles.group_button}>
+					<div>
+						<Button p_12_20 grey rounded_6>
+							Hủy bỏ
+						</Button>
+					</div>
+					{/* <FormContext.Consumer>
+						{({onSubmit}) => ( */}
+					<div className={styles.btn}>
+						<Button p_12_20 primary rounded_6 icon={<FolderOpen size={18} color='#fff' />}>
+							Lưu lại
+						</Button>
+					</div>
+					{/* )}
+					</FormContext.Consumer> */}
+				</div>
+				<div className={styles.close}>
+					<IoClose size={28} color='#8492A6' />
+				</div>
+			</div>
+		</Form>
+	);
+}
 
 export default CreateGroupContractor;
