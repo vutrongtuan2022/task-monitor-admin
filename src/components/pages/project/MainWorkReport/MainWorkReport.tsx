e559b0fbc263fdfc2f03ba0be1dc2c0c1756b557
HTN-UnHappyCODE0 <2051063590@e.tlu.edu.vn>
2024-10-03 13:35:04 +0700

    ff

--- src/components/pages/project/MainWorkReport/MainWorkReport.tsx
+++ src/components/pages/project/MainWorkReport/MainWorkReport.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
 
-import { PropsMainWorkReport } from './interfaces';
+import {PropsMainWorkReport} from './interfaces';
 import styles from './MainWorkReport.module.scss';
 import LayoutPages from '~/components/layouts/LayoutPages';
-import { PATH } from '~/constants/config';
-import { useRouter } from 'next/router';
+import {PATH} from '~/constants/config';
+import {useRouter} from 'next/router';
 import Button from '~/components/common/Button';
 import Image from 'next/image';
 import icons from '~/constants/images/icons';
+import Progress from '~/components/common/Progress';
+import {clsx} from 'clsx';
+import GridColumn from '~/components/layouts/GridColumn';
+import Search from '~/components/common/Search';
+import FilterCustom from '~/components/common/FilterCustom';
+import WrapperScrollbar from '~/components/layouts/WrapperScrollbar';
+import DataWrapper from '~/components/common/DataWrapper';
+import Table from '~/components/common/Table';
+import StateActive from '~/components/common/StateActive';
+import Link from 'next/link';
+import DateRangerCustom from '~/components/common/DateRangerCustom';
+import Pagination from '~/components/common/Pagination';
 
-function MainWorkReport({ }: PropsMainWorkReport) {
+function MainWorkReport({}: PropsMainWorkReport) {
 	const router = useRouter();
 
-	const { _uuid } = router.query;
+	const {_uuid} = router.query;
 
 	return (
 		<LayoutPages
@@ -36,43 +48,153 @@ function MainWorkReport({ }: PropsMainWorkReport) {
 			]}
 			action={
 				<div className={styles.group_btn}>
-
-					<Button
-						p_14_24
-						rounded_8
-						primary
-						href={''}
-						onClick={() => { }}
-					// icon={<Image alt='icon add' src={icons.iconAdd} width={20} height={20} />}
-					>
+					<Button p_14_24 rounded_8 primary>
 						Kết thúc dự án
 					</Button>
-
-					<Button
-						p_14_24
-						rounded_8
-						light-red
-						href={''}
-						onClick={() => { }}
-					// icon={<Image alt='icon add' src={icons.iconAdd} width={20} height={20} />}
-					>
+					<Button p_14_24 rounded_8 light-red>
 						Xóa
 					</Button>
-
-					<Button
-						p_14_24
-						rounded_8
-						primaryLinear
-						href={''}
-						onClick={() => { }}
-					// icon={<Image alt='icon add' src={icons.iconAdd} width={20} height={20} />}
-					>
+					<Button p_14_24 rounded_8 primaryLinear>
 						Chỉnh sửa
 					</Button>
 				</div>
 			}
 		>
-			<div className={styles.basic_info}></div>
+			<div className={styles.basic_info}>
+				<div className={styles.head}>
+					<h4>Tiến độ công việc</h4>
+				</div>
+				<div className={styles.progress_group}>
+					<GridColumn col_3>
+						<div className={styles.progress}>
+							<p>Trong tháng</p>
+							<div className={styles.progress_label}>
+								<Progress percent={80} width={120} isPercent={false} />
+								<div>
+									<span className={styles.value}>9</span>/10
+								</div>
+							</div>
+						</div>
+						<div className={styles.progress}>
+							<p>Trong năm</p>
+							<div className={styles.progress_label}>
+								<Progress percent={80} width={120} isPercent={false} />
+								<div>
+									<span className={styles.value}>9</span>/10
+								</div>
+							</div>
+						</div>
+						<div className={styles.progress}>
+							<p>Trong dự án</p>
+							<div className={styles.progress_label}>
+								<Progress percent={80} width={120} isPercent={false} />
+								<div>
+									<span className={styles.value}>9</span>/10
+								</div>
+							</div>
+						</div>
+					</GridColumn>
+				</div>
+			</div>
+			<div className={clsx(styles.basic_info, styles.mt)}>
+				<div className={styles.head}>
+					<h4>Tiến độ công việc</h4>
+				</div>
+				<div className={styles.container}>
+					<div className={styles.head_filt}>
+						<div className={styles.main_search}>
+							<div className={styles.search}>
+								<Search keyName='_keyword' placeholder='Tìm kiếm theo tên công việc' />
+							</div>
+							<div className={styles.filter}>
+								<DateRangerCustom titleTime='Thời gian' />
+							</div>
+							<div className={styles.filter}>
+								<FilterCustom
+									isSearch
+									name='Trạng thái'
+									query='_'
+									listFilter={[
+										{
+											id: 1,
+											name: '1',
+										},
+										{
+											id: 1,
+											name: '2',
+										},
+									]}
+								/>
+							</div>
+						</div>
+					</div>
+					<WrapperScrollbar>
+						<DataWrapper data={[1, 1, 1]}>
+							<Table
+								data={[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]}
+								column={[
+									{
+										title: 'STT',
+										render: (data: any, index: number) => <>{index + 1}</>,
+									},
+									{
+										title: 'Tên công việc',
+										render: (data: any) => <>{'---'}</>,
+									},
+									{
+										title: 'Giai đoạn thực hiện',
+										render: (data: any) => <>{'---'}</>,
+									},
+									{
+										title: 'Megatype',
+										render: (data: any) => <>{'---'}</>,
+									},
+									{
+										title: 'Công việc bổ sung',
+										render: (data: any) => <>{'---'}</>,
+									},
+									{
+										title: 'Người báo cáo',
+										render: (data: any) => <>{'---'}</>,
+									},
+									{
+										title: 'Trạng thái',
+										render: (data: any) => (
+											<div className={styles.state}>
+												<StateActive
+													stateActive={2}
+													listState={[
+														{
+															state: 1,
+															text: 'Đã báo cáo',
+															textColor: '#fff',
+															backgroundColor: '#06D7A0',
+														},
+														{
+															state: 2,
+															text: 'Bị từ chối',
+															textColor: '#fff',
+															backgroundColor: '#F37277',
+														},
+													]}
+												/>
+												<Link href={``} className={styles.link_state}>
+													Gửi lại
+												</Link>
+											</div>
+										),
+									},
+									{
+										title: 'Tình trạng',
+										render: (data: any) => <div></div>,
+									},
+								]}
+							/>
+							<Pagination currentPage={1} pageSize={20} total={20} />
+						</DataWrapper>
+					</WrapperScrollbar>
+				</div>
+			</div>
 		</LayoutPages>
 	);
 }
