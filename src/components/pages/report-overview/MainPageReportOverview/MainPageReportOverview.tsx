d4bb2e4ce579afe8d055de2b72eefd39b218105b
TuanVT <vutrongtuan@gmail.com>
2024-10-17 09:34:53 +0700

    ff

---
+++ src/components/pages/report-overview/MainPageReportOverview/MainPageReportOverview.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import {PropsMainPageReportOverview} from './interfaces';
+function MainPageReportOverview() {
+	return <div>MainPageReportOverview</div>;
+}
+
+export default MainPageReportOverview;
