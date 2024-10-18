2b0e1e53ccd04c95a1c8a6a05ae2eef8885df294
HTN-UnHappyCODE0 <2051063590@e.tlu.edu.vn>
2024-10-18 14:49:59 +0700

    ff

--- src/components/pages/project/MainInfoProject/MainInfoProject.tsx
+++ src/components/pages/project/MainInfoProject/MainInfoProject.tsx
@@ -342,11 +342,11 @@ function MainInfoProject({}: PropsMainInfoProject) {
 									<p>Số tiền giải ngân lũy kế theo năm</p>
 									<p>{convertCoin(detailProject?.annualAccumAmount!)} VND</p>
 								</div>
-								{/* <div className={styles.line}></div>
+								<div className={styles.line}></div>
 								<div className={styles.item_capital}>
 									<p>Số tiền giải ngân lũy kế đến hiện tại</p>
-									<p>{'---'} VND</p>
-								</div> */}
+									<p>{convertCoin(detailProject?.accumAmount!)} VND</p>
+								</div>
 							</div>
 						</div>
 					</div>
