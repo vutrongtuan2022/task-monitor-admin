4a650f1f533845ff098319ad2b56a5203316660b
truongdb <engtruong1609@gmail.com>
2024-12-04 11:49:33 +0700

    update notify

--- src/constants/config/enum.ts
+++ src/constants/config/enum.ts
@@ -65,6 +65,8 @@ export enum QUERY_KEY {
 	detail_progress_contract_fund_project,
 
 	tree_task,
+	count_unseen_noti,
+	list_notify,
 }
 
 export enum TYPE_DATE {
@@ -174,4 +176,17 @@ export enum TYPE_WORK {
 export enum SORT_TYPE {
 	DECREASE = 1,
 	INCREASE,
+}
+
+export enum STATE_NOTIFY {
+	NOT_READ,
+	READ,
+}
+
+export enum TYPE_NOTIFY {
+	PROJECT = 1,
+	REPORT,
+	CONTRACT,
+	OVERVIEW,
+	CONTRACT_FUND,
 }
